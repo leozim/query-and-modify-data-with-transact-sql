55cdacdc793d2f547d42884698cc87cbb4c79e83
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2025-12-17 14:29:40 -0300

    criando query com inner e outer joins

---
+++ 03  CombineMultipleTablesWithJOINsInT-SQL/QueryMultipleTablesWithJoins.sql
@@ -0,0 +1,125 @@
+
+/* INNER JOIN */
+
+SELECT
+	SalesLT.Product.Name AS ProductName,
+	SalesLT.ProductCategory.Name AS Category
+FROM
+	SalesLT.Product
+INNER JOIN
+	SalesLT.ProductCategory
+	ON SaLesLT.Product.ProductCategoryID = SalesLT.ProductCategory.ProductCategoryID;
+
+-- SAME RESULT BECAUSE IT IS AN INNER JOIN. THE ORDER OF THE INSTANCES TABLE DOESN'T CARE!
+
+SELECT
+	SalesLT.Product.Name AS ProductName,
+	SalesLT.ProductCategory.Name AS Category
+FROM
+	SalesLT.ProductCategory
+INNER JOIN
+	SalesLT.Product
+	ON SalesLT.ProductCategory.ProductCategoryID = SaLesLT.Product.ProductCategoryID;
+
+-- THE RESULT SHOULD BE THE SAME ONCE 'INNER' IS DEFAULT WHEN JOINs TABLES
+
+SELECT
+	SalesLT.Product.Name AS ProductName,
+	SalesLT.ProductCategory.Name AS Category
+FROM
+	SalesLT.Product
+JOIN
+	SalesLT.ProductCategory
+	ON SaLesLT.Product.ProductCategoryID = SalesLT.ProductCategory.ProductCategoryID;
+
+-- STILL RETURN THE SAME RESULT. JUST USING ALIAS QUERYING
+
+SELECT
+	p.Name AS ProductName,
+	c.Name AS Category
+FROM
+	SalesLT.Product AS p
+INNER JOIN
+	SalesLT.ProductCategory AS c
+	ON p.ProductCategoryID = c.ProductCategoryID;
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+SELECT
+	oh.OrderDate,
+	oh.PurchaseOrderNumber,
+	p.Name AS ProductName,
+	od.OrderQty,
+	od.UnitPrice,
+	od.OrderQty * od.UnitPrice AS TotalPrice
+FROM 
+	SalesLT.SalesOrderHeader AS oh
+JOIN
+	SalesLT.SalesOrderDetail AS od
+		ON od.SalesOrderID = oh.SalesOrderID
+JOIN
+	SalesLT.Product AS p
+		ON od.ProductID = p.ProductID
+ORDER BY
+	oh.OrderDate,
+	oh.SalesOrderID,
+	od.SalesOrderDetailID;
+----------------------------------------------------------------------------------
+
+/* OUTER JOIN */
+SELECT
+	c.FirstName,
+	c.LastName,
+	oh.PurchaseOrderNumber
+FROM
+	SalesLT.Customer AS c
+LEFT OUTER JOIN
+	SalesLT.SalesOrderHeader AS oh
+		ON c.CustomerID =  oh.CustomerID
+ORDER BY
+	c.CustomerID;
+
+-- SHOULD RETURN SAME RETURN AS ABOVE
+SELECT
+	c.FirstName,
+	c.LastName,
+	oh.PurchaseOrderNumber
+FROM
+	SalesLT.Customer AS c
+LEFT JOIN
+	SalesLT.SalesOrderHeader AS oh
+		ON c.CustomerID =  oh.CustomerID
+ORDER BY
+	c.CustomerID;
+
+-- RETURN ONLY THE CUSTOMERS WHO HAVEN'T PLACED ANY ORDERS.
+SELECT
+	c.FirstName,
+	c.LastName,
+	oh.PurchaseOrderNumber
+FROM
+	SalesLT.Customer AS c
+LEFT JOIN
+	SalesLT.SalesOrderHeader AS oh
+		ON c.CustomerID = oh.CustomerID
+WHERE
+	oh.SalesOrderNumber IS NULL
+ORDER BY
+	c.CustomerID;
+
+-- RETRIEVE DATA FROM THREE TABLES
+SELECT
+	p.Name AS ProductName,
+	oh.PurchaseOrderNumber
+FROM
+	SalesLT.Product AS p
+LEFT JOIN
+	SalesLT.SalesOrderDetail AS od
+		ON p.ProductID = od.ProductID
+LEFT JOIN
+	SalesLT.SalesOrderHeader AS oh
+		ON od.SalesOrderID = oh.SalesOrderID
+ORDER BY
+	p.ProductID;
+
+/* CROSS JOIN */
+/* SELF JOIN */
