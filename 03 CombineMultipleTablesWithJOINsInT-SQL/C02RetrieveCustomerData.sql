7ba5fa8b89b5ef231df453f2a2b4f86012c3ed15
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2025-12-19 11:21:20 -0300

    adicionando descrição dos desafios como comentários

--- 03 CombineMultipleTablesWithJOINsInT-SQL/C02RetrieveCustomerData.sql
+++ 03 CombineMultipleTablesWithJOINsInT-SQL/C02RetrieveCustomerData.sql
@@ -1,1 +1,17 @@
--- Challenge 02
+/* CHALLENGE 02 */
+
+-- 1 Retrieve a list of all customers and their orders
+/*
+The sales manager wants a list of all customer companies and their contacts (first name and last name), 
+showing the purchase order number and total due for each order they have placed. 
+Customers who have not placed any orders should be included at the bottom of the list with NULL values 
+for the purchase order number and total due.
+*/
+
+-- 2 Retrieve a list of customers with no address
+/*
+A sales employee has noticed that Adventure Works does not have address information for all customers. 
+You must write a query that returns a list of customer IDs, company names, 
+contact names (first name and last name), and phone numbers for customers 
+with no address stored in the database.
+*/
--- 03 CombineMultipleTablesWithJOINsInT-SQL/C03CreateProductCatalog.sql
+++ 03 CombineMultipleTablesWithJOINsInT-SQL/C03CreateProductCatalog.sql
@@ -1,1 +1,10 @@
--- Challenge 03
+/* CHALLENGE 03 */
+
+-- 1 Retrieve product information by category
+/*
+	The product catalog will list products by parent category and subcategory, 
+	so you must write a query that retrieves the parent category name, 
+	subcategory name, and product name fields for the catalog.
+*/
+
+
